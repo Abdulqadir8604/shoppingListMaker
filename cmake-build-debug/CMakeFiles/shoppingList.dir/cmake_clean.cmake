7758527cfb3a9aa8d4c8a76c4f9e6689aa60d5a8
Abdulqadir8604 <76737223+Abdulqadir8604@users.noreply.github.com>
2021-05-24 15:40:54 +0530

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/shoppingList.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shoppingList.dir/main.cpp.obj"
+  "libshoppingList.dll.a"
+  "shoppingList.exe"
+  "shoppingList.exe.manifest"
+  "shoppingList.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shoppingList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+const int n = 5;
+string items[n];
+bool rep;
+
+void check(){
+    for (int z = 0; z < n; ++z) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if(i!=j){
+                    if(items[i] == items[j]){
+                        rep = true;
+                        items[i]=items[i+1];
+                    }
+                }
+            }
+        }
+    }
+    }
+
+int main() {
+
+    cout<< "If you are over with your list then just keep on pressing enter :)"<<endl;
+    cout<<"Enter items: ";
+    int f = 0;
+    do {
+        for (int i = 0; i < n; ++i) {
+            cin >> items[i];
+        }
+        f++;
+    } while (f<1);
+    check();
+
+    for (int i = 0; i < n; ++i) {
+        cout<<items[i]<<endl;
+    }
+
+    ofstream myfile ("items.txt");
+    if (myfile.is_open())
+    {
+        myfile << "My list: \n";
+        myfile << "\n";
+        for (auto & item : items) {
+            myfile << item << endl;
+        }
+        if (rep){
+            myfile << "There were some items that were repeated so i deleted it...";
+        }
+        myfile.close();
+
+    }
+    else cout << "Unable to open file";
+    return 0;
+}
+
